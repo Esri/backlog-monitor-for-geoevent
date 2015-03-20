f5a01b4c8fd12395daff49a6b0baae25db2e350c
Ryan Elliott <relliott@esri.com>
2015-03-20 09:57:48 -0700

    Modified the monitoring code to connect to RabbitMQ, which is the new message but for GeoEvent 10.3.x

--- src/main/java/com/esri/ges/monitor/Monitor.java
+++ src/main/java/com/esri/ges/monitor/Monitor.java
@@ -14,10 +14,10 @@ import javax.management.remote.JMXServiceURL;
 
 public class Monitor
 {
-	private MBeanServerConnection mbsc;
 	private JMXConnector jmxc;
-	String username = "arcgis";
-	String password = "manager";
+	String username = "admin";
+	String password = "admin";
+	private RabbitMQManager rabbitMQManager;
 
 	public Monitor(String[] args ) throws Exception
 	{
@@ -39,20 +39,7 @@ public class Monitor
 
 	private void connect() throws IOException
 	{
-		Map<String, String[]> env = new HashMap<String, String[]>();
-		String[] credentials = new String[] { username, password };
-		env.put(JMXConnector.CREDENTIALS, credentials);
-
-		JMXServiceURL url;
-		try
-		{
-			url = new JMXServiceURL("service:jmx:rmi://0.0.0.0:44445/jndi/rmi://0.0.0.0:1100/karaf-root");
-		} catch (MalformedURLException e)
-		{
-			throw new IOException(e.getMessage());
-		}
-		jmxc = JMXConnectorFactory.connect(url, env);
-		mbsc = jmxc.getMBeanServerConnection();
+		rabbitMQManager = new RabbitMQManager( "localhost", 6080, username, password );
 	}
 
 	public void close() throws IOException
@@ -76,42 +63,7 @@ public class Monitor
 
 	public Map<String,Long> getCounts() throws Exception
 	{
-		HashMap<String,Long> results = new HashMap<>();
-
-		try
-		{
-			if( mbsc == null )
-				connect();
-
-			// Collect input Queues
-			ObjectName queueNamePrefix = new ObjectName("org.apache.activemq:BrokerName=localhost,Type=Queue,*");
-			Set<ObjectName> intputNames = mbsc.queryNames(queueNamePrefix, null);
-			for( ObjectName inputBeanName : intputNames )
-			{
-				String inputName = (String) mbsc.getAttribute(inputBeanName, "Name");
-				if( inputName.equals("incidents"))
-					continue;
-				long inflightCount = (Long) mbsc.getAttribute(inputBeanName, "InFlightCount");
-				results.put("Input:"+inputName, inflightCount);
-			}
-
-			// Collect output Topics
-			ObjectName topicNamePrefix = new ObjectName("org.apache.activemq:BrokerName=localhost,Type=Topic,*");
-			Set<ObjectName> outputNames = mbsc.queryNames(topicNamePrefix, null);
-			for( ObjectName outputBeanName : outputNames )
-			{
-				String outputName = (String) mbsc.getAttribute(outputBeanName, "Name");
-				if( outputName.startsWith("ActiveMQ.Advisory"))
-					continue;
-				long inflightCount = (Long) mbsc.getAttribute(outputBeanName, "InFlightCount");
-				results.put("Output:"+outputName, inflightCount);
-			}
-		}catch(IOException ex)
-		{
-			mbsc = null;
-			throw ex;
-		}
-
+		Map<String,Long> results = rabbitMQManager.getCounts();
 		return results;
 	}
 
